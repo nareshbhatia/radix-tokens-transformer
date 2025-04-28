c736333e358059ab213ed467c305fe467dec7a5d
Naresh Bhatia <me@nareshbhatia.dev>
2025-04-27 23:34:47 -0400

    modify rgb CSS variable format

--- codegen/transform-w3c-tokens-to-platforms/index.ts
+++ codegen/transform-w3c-tokens-to-platforms/index.ts
@@ -77,7 +77,7 @@ StyleDictionary.registerTransform({
       const r = parseInt(hex.substring(0, 2), 16);
       const g = parseInt(hex.substring(2, 4), 16);
       const b = parseInt(hex.substring(4, 6), 16);
-      return `${r},${g},${b}`;
+      return `${r}, ${g}, ${b}`;
     }
 
     // Return the token value as is
